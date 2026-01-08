816341c0068939e53eb8eb78d7eb4f8ae7dbe34b
kwakseobang <iii6602@naver.com>
2026-01-08 19:05:26 +0900

    Setting[#1]: MVC, 통합, 단위 테스트를 위한 테스트 지원 클래스 추가

---
+++ src/test/java/org/kwakmunsu/haruhana/ControllerTestSupport.java
@@ -0,0 +1,22 @@
+package org.kwakmunsu.haruhana;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import tools.jackson.databind.ObjectMapper;
+
+@ActiveProfiles("test")
+@WebMvcTest(
+        controllers = {
+
+        })
+public abstract class ControllerTestSupport {
+
+    @Autowired
+    protected MockMvcTester mvcTester;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+}
