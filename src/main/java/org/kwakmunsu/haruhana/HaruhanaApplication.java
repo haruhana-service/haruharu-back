bbe04a65bb1038f09581cd30eac23c1e2c320c4d
kwakseobang <iii6602@naver.com>
2026-01-08 16:15:42 +0900

    initialize

---
+++ src/main/java/org/kwakmunsu/haruhana/HaruhanaApplication.java
@@ -0,0 +1,13 @@
+package org.kwakmunsu.haruhana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruhanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaruhanaApplication.class, args);
+    }
+
+}
