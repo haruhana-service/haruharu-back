bee57f043d1324cecb43b51fa5b50ada77070e64
kwakseobang <iii6602@naver.com>
2026-01-11 19:41:36 +0900

    Docs[#4]: 회원 학습 정보 등록 API Swagger 작성

--- src/main/java/org/kwakmunsu/haruhana/domain/member/controller/MemberDocsController.java
+++ src/main/java/org/kwakmunsu/haruhana/domain/member/controller/MemberDocsController.java
@@ -4,10 +4,13 @@ import static org.kwakmunsu.haruhana.global.support.error.ErrorType.BAD_REQUEST;
 import static org.kwakmunsu.haruhana.global.support.error.ErrorType.DEFAULT_ERROR;
 import static org.kwakmunsu.haruhana.global.support.error.ErrorType.DUPLICATE_LOGIN_ID;
 import static org.kwakmunsu.haruhana.global.support.error.ErrorType.DUPLICATE_NICKNAME;
+import static org.kwakmunsu.haruhana.global.support.error.ErrorType.FORBIDDEN_ERROR;
+import static org.kwakmunsu.haruhana.global.support.error.ErrorType.NOT_FOUND_CATEGORY;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.kwakmunsu.haruhana.domain.member.controller.dto.MemberCreateRequest;
+import org.kwakmunsu.haruhana.domain.member.controller.dto.PreferenceRegisterRequest;
 import org.kwakmunsu.haruhana.global.support.response.ApiResponse;
 import org.kwakmunsu.haruhana.global.swagger.ApiExceptions;
 import org.springframework.http.ResponseEntity;
@@ -30,5 +33,27 @@ public abstract class MemberDocsController {
             DEFAULT_ERROR
     })
     public abstract ResponseEntity<ApiResponse<Long>> create(MemberCreateRequest request);
+
+    @Operation(
+            summary = "회원 학습 정보 등록 - JWT [O]",
+            description = """
+                    ### 회원의 학습 정보를 등록합니다.
+                    - 카테고리 주제 ID와 난이도를 포함한 요청을 받습니다.
+                    - 회원가입을 마친 GUEST 회원이 자신의 학습 정보를 등록할 때 사용합니다.
+                    - GUEST 회원만 접근할 수 있습니다.
+                    - 성공 시 빈 응답을 반환합니다.
+                    - 첫 등록 시 ROLE_MEMBER로 권한이 변경됩니다.
+                    """
+    )
+    @ApiExceptions(values = {
+            BAD_REQUEST,
+            FORBIDDEN_ERROR,
+            NOT_FOUND_CATEGORY,
+            DEFAULT_ERROR
+    })
+    public abstract ResponseEntity<ApiResponse<?>> registerPreference(
+            PreferenceRegisterRequest request,
+            Long memberId
+    );
 
 }
