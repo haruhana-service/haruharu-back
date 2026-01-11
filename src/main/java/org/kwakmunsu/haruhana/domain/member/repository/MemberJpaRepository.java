923561c32228be73acdd10d83998cb3fcca19f08
kwakseobang <iii6602@naver.com>
2026-01-11 15:01:16 +0900

    Feat[#5]: 회원 중복 검증 로직에 상태 기반 체크 추가

--- src/main/java/org/kwakmunsu/haruhana/domain/member/repository/MemberJpaRepository.java
+++ src/main/java/org/kwakmunsu/haruhana/domain/member/repository/MemberJpaRepository.java
@@ -1,11 +1,14 @@
 package org.kwakmunsu.haruhana.domain.member.repository;
 
+import java.util.Optional;
 import org.kwakmunsu.haruhana.domain.member.entity.Member;
+import org.kwakmunsu.haruhana.global.entity.EntityStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
 
-    boolean existsByLoginId(String loginId);
-    boolean existsByNickname(String nickname);
+    boolean existsByLoginIdAndStatus(String loginId, EntityStatus status);
+    boolean existsByNicknameAndStatus(String nickname, EntityStatus status);
+    Optional<Member> findByLoginIdAndStatus(String loginId, EntityStatus status);
 
 }
