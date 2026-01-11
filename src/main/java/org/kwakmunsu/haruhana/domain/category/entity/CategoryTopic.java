ac335def9fe0272cfc5a3a8e4f61057b77cf6196
kwakseobang <iii6602@naver.com>
2026-01-11 19:30:59 +0900

    Feat[#4]: Category 관련 Entity 속성 및 메서드 추가

--- src/main/java/org/kwakmunsu/haruhana/domain/category/entity/CategoryTopic.java
+++ src/main/java/org/kwakmunsu/haruhana/domain/category/entity/CategoryTopic.java
@@ -1,5 +1,6 @@
 package org.kwakmunsu.haruhana.domain.category.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -10,5 +11,24 @@ import org.kwakmunsu.haruhana.global.entity.BaseEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class CategoryTopic extends BaseEntity {
+
+    @Column(nullable = false)
+    private Long groupId;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public static CategoryTopic create(Long groupId, String name) {
+        CategoryTopic categoryTopic = new CategoryTopic();
+
+        categoryTopic.groupId = groupId;
+        categoryTopic.name = name;
+
+        return categoryTopic;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
 
 }
