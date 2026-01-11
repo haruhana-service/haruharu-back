bd2dd6234916447c1ade91f3305cbec3c50bef28
kwakseobang <iii6602@naver.com>
2026-01-11 14:15:04 +0900

    Test[#5]: Test용 Security 환경 및 커스텀 어노테이션 생성

---
+++ src/test/java/org/kwakmunsu/haruhana/security/TestMemberSecurityContextFactory.java
@@ -0,0 +1,32 @@
+package org.kwakmunsu.haruhana.security;
+
+import java.util.Collections;
+import lombok.extern.slf4j.Slf4j;
+import org.kwakmunsu.haruhana.security.annotation.TestMember;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+@Slf4j
+public class TestMemberSecurityContextFactory implements WithSecurityContextFactory<TestMember> {
+
+    @Override
+    public SecurityContext createSecurityContext(TestMember annotation) {
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + annotation.role());
+        String memberId = String.valueOf(annotation.id());
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                memberId,
+                null,
+                Collections.singletonList(authority)
+        );
+        securityContext.setAuthentication(authentication);
+
+        return securityContext;
+    }
+
+}
