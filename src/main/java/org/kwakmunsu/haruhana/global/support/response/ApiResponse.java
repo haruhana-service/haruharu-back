69ebbeb685a87cbdb8fa72e1f4a97af707d2ccc1
kwakseobang <iii6602@naver.com>
2026-01-11 12:31:25 +0900

    Docs[#3]: Api 공통 응답 Swagger Schema 추가

--- src/main/java/org/kwakmunsu/haruhana/global/support/response/ApiResponse.java
+++ src/main/java/org/kwakmunsu/haruhana/global/support/response/ApiResponse.java
@@ -1,10 +1,17 @@
 package org.kwakmunsu.haruhana.global.support.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.kwakmunsu.haruhana.global.support.error.ErrorType;
 
+@Schema(description = "API 공통 응답")
 public record ApiResponse<T>(
+        @Schema(description = "응답 결과 유형", example = "SUCCESS")
         ResultType result,
+
+        @Schema(description = "응답 데이터")
         T data,
+
+        @Schema(description = "에러 정보")
         ErrorMessage error
 ) {
 
