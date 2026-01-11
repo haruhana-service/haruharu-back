a92ace69a20e736d46b136001aebfe0a8624a844
kwakseobang <iii6602@naver.com>
2026-01-11 19:30:59 +0900

    Feat[#4]: Category 관련 Entity 속성 및 메서드 추가

--- src/main/java/org/kwakmunsu/haruhana/domain/category/entity/Category.java
+++ src/main/java/org/kwakmunsu/haruhana/domain/category/entity/Category.java
@@ -1,5 +1,6 @@
 package org.kwakmunsu.haruhana.domain.category.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -10,5 +11,20 @@ import org.kwakmunsu.haruhana.global.entity.BaseEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Category extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public static Category create(String name) {
+        Category category = new Category();
+
+        category.name = name;
+
+        return category;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
 
 }
--- src/main/java/org/kwakmunsu/haruhana/domain/category/entity/CategoryGroup.java
+++ src/main/java/org/kwakmunsu/haruhana/domain/category/entity/CategoryGroup.java
@@ -1,5 +1,6 @@
 package org.kwakmunsu.haruhana.domain.category.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -10,5 +11,24 @@ import org.kwakmunsu.haruhana.global.entity.BaseEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class CategoryGroup extends BaseEntity {
+
+    @Column(nullable = false)
+    private Long categoryId;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public static CategoryGroup create(Long categoryId, String name) {
+        CategoryGroup categoryGroup = new CategoryGroup();
+
+        categoryGroup.categoryId = categoryId;
+        categoryGroup.name = name;
+
+        return categoryGroup;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
 
 }
