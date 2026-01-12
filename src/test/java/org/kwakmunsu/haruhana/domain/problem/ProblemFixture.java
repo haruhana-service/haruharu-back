2d69ad5db323e76299f6d9464bca800f18b8ea59
kwakseobang <iii6602@naver.com>
2026-01-12 15:24:26 +0900

    Refactor[#15]: Test Fixture로 코드 분리 후 관련 테스트 코드 수정

--- src/test/java/org/kwakmunsu/haruhana/domain/problem/ProblemFixture.java
+++ src/test/java/org/kwakmunsu/haruhana/domain/problem/ProblemFixture.java
@@ -1,23 +1,69 @@
 package org.kwakmunsu.haruhana.domain.problem;
 
 import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.kwakmunsu.haruhana.domain.category.entity.CategoryTopic;
 import org.kwakmunsu.haruhana.domain.problem.entity.Problem;
 import org.kwakmunsu.haruhana.domain.problem.enums.ProblemDifficulty;
-import org.kwakmunsu.haruhana.domain.problem.service.Prompt;
+import org.springframework.test.util.ReflectionTestUtils;
 
-public class ProblemFixture {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ProblemFixture {
 
-    public static Problem createProblem() {
-        return Problem.create(
-                "문제 제목",
-                "문제 내용",
-                "문제 답안",
-                CategoryTopic.create(1L, "카테고리 토픽 이름"),
-                ProblemDifficulty.MEDIUM,
+    public static final String TITLE = "테스트 문제";
+    public static final String DESCRIPTION = "테스트 설명";
+    public static final String AI_ANSWER = "AI 답변";
+    public static final ProblemDifficulty DIFFICULTY = ProblemDifficulty.MEDIUM;
+    public static final String PROMPT_VERSION = "V1_PROMPT";
+
+    public static Problem createProblem(CategoryTopic categoryTopic) {
+        Problem problem = Problem.create(
+                TITLE,
+                DESCRIPTION,
+                AI_ANSWER,
+                categoryTopic,
+                DIFFICULTY,
                 LocalDate.now(),
-                Prompt.V1_PROMPT.name()
+                PROMPT_VERSION
         );
+        ReflectionTestUtils.setField(problem, "id", 1L);
+        return problem;
+    }
+
+    public static Problem createProblem(Long id, CategoryTopic categoryTopic) {
+        Problem problem = Problem.create(
+                TITLE,
+                DESCRIPTION,
+                AI_ANSWER,
+                categoryTopic,
+                DIFFICULTY,
+                LocalDate.now(),
+                PROMPT_VERSION
+        );
+        ReflectionTestUtils.setField(problem, "id", id);
+        return problem;
+    }
+
+    public static Problem createProblem(
+            Long id,
+            String title,
+            String description,
+            CategoryTopic categoryTopic,
+            ProblemDifficulty difficulty
+    ) {
+        Problem problem = Problem.create(
+                title,
+                description,
+                AI_ANSWER,
+                categoryTopic,
+                difficulty,
+                LocalDate.now(),
+                PROMPT_VERSION
+        );
+        ReflectionTestUtils.setField(problem, "id", id);
+        return problem;
     }
 
 }
+
