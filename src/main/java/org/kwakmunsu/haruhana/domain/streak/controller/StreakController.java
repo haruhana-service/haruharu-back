1c8a92dc8bcf1dc8e1c7e61fba1b9eb6ac67ca7c
kwakseobang <iii6602@naver.com>
2026-01-15 12:20:37 +0900

    Feat[#23]: 스트릭 조회 기능 구현 및 관련 테스트 추가

--- src/main/java/org/kwakmunsu/haruhana/domain/streak/controller/StreakController.java
+++ src/main/java/org/kwakmunsu/haruhana/domain/streak/controller/StreakController.java
@@ -1,10 +1,26 @@
 package org.kwakmunsu.haruhana.domain.streak.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.kwakmunsu.haruhana.domain.streak.service.StreakService;
+import org.kwakmunsu.haruhana.domain.streak.service.dto.response.StreakResponse;
+import org.kwakmunsu.haruhana.global.annotation.LoginMember;
+import org.kwakmunsu.haruhana.global.support.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class StreakController extends StreakDocsController {
+
+    private final StreakService streakService;
+
+    @Override
+    @GetMapping("/v1/streaks")
+    public ResponseEntity<ApiResponse<StreakResponse>> getStreak(@LoginMember Long memberId) {
+        StreakResponse response = streakService.getStreak(memberId);
+
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
 
 }
