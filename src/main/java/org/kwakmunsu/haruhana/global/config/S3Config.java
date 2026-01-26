18fcb20a6e4a9644f05e7cd0aa5639de06993f1b
kwakseobang <iii6602@naver.com>
2026-01-26 12:07:51 +0900

    Refactor[#52]: S3Client 및 S3Presigner에 공통 AwsCredentialsProvider 빈 주입하도록 변경

--- src/main/java/org/kwakmunsu/haruhana/global/config/S3Config.java
+++ src/main/java/org/kwakmunsu/haruhana/global/config/S3Config.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -21,22 +22,23 @@ public class S3Config {
     private String region;
 
     @Bean
-    public S3Client s3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        return () -> AwsBasicCredentials.create(accessKey, secretKey);
+    }
 
+    @Bean
+    public S3Client s3Client(AwsCredentialsProvider awsCredentialsProvider) {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(() -> credentials)
+                .credentialsProvider(awsCredentialsProvider)
                 .build();
     }
 
     @Bean
-    public S3Presigner s3Presigner() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-
+    public S3Presigner s3Presigner(AwsCredentialsProvider awsCredentialsProvider) {
         return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(() -> credentials)
+                .credentialsProvider(awsCredentialsProvider)
                 .build();
     }
 
