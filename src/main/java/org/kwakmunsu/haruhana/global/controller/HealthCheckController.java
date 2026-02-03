ee803245bda11fae2db082b9e9b15a6746b6f127
kwakseobang <iii6602@naver.com>
2026-02-03 11:22:47 +0900

    Refactor HealthCheckController to optimize import statements

--- src/main/java/org/kwakmunsu/haruhana/global/controller/HealthCheckController.java
+++ src/main/java/org/kwakmunsu/haruhana/global/controller/HealthCheckController.java
@@ -1,11 +1,10 @@
 package org.kwakmunsu.haruhana.global.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class HealthCheckController {
