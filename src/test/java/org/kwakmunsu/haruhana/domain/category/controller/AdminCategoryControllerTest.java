e458b37e4126e3e84c90389ba7d7c4e1d79e1290
kwakseobang <iii6602@naver.com>
2026-02-05 11:41:58 +0900

    Test[#60]: AdminCategoryController 및 CategoryManager 테스트 추가
    - AdminCategoryController 테스트 케이스 작성
    - CategoryManager 통합 테스트 케이스 작성 및 예외 상황 검증 추가

---
+++ src/test/java/org/kwakmunsu/haruhana/domain/category/controller/AdminCategoryControllerTest.java
@@ -0,0 +1,63 @@
+package org.kwakmunsu.haruhana.domain.category.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+import org.kwakmunsu.haruhana.ControllerTestSupport;
+import org.kwakmunsu.haruhana.domain.category.controller.dto.request.CategoryGroupCreateRequest;
+import org.kwakmunsu.haruhana.domain.category.controller.dto.request.CategoryCreateRequest;
+import org.kwakmunsu.haruhana.domain.category.controller.dto.request.CategoryTopicCreateRequest;
+import org.kwakmunsu.haruhana.security.annotation.TestAdmin;
+import org.springframework.http.MediaType;
+
+class AdminCategoryControllerTest extends ControllerTestSupport {
+
+    @TestAdmin
+    @Test
+    void 카테고리를_생성한다() throws JsonProcessingException {
+        // given
+        var request = new CategoryCreateRequest("알고리즘");
+
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        // when & then
+        assertThat(mvcTester.post().uri("/v1/admin/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .apply(print())
+                .hasStatusOk();
+    }
+
+    @TestAdmin
+    @Test
+    void 카테고리_그룹을_생성한다() throws Exception {
+        // given
+        var request = new CategoryGroupCreateRequest(1L, "자료구조");
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        // when & then
+        assertThat(mvcTester.post().uri("/v1/admin/categories/groups")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .apply(print())
+                .hasStatusOk();
+    }
+
+    @TestAdmin
+    @Test
+    void 카테고리_토픽을_생성한다() throws Exception {
+        // given
+        var request = new CategoryTopicCreateRequest(1L, "배열");
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        // when & then
+        assertThat(mvcTester.post().uri("/v1/admin/categories/topics")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .apply(print())
+                .hasStatusOk();
+    }
+
+}
