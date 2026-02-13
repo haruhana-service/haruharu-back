10ea8fe0e7767041e7359f1d2297ed91a5cdefe9
kwakseobang <iii6602@naver.com>
2026-02-13 11:23:05 +0900

    Feat[#87]: 디바이스 토큰 삭제 로직 추가 및 FCM 토큰 검증 에러 타입 확장

---
+++ src/main/java/org/kwakmunsu/haruhana/domain/member/service/MemberDeviceValidator.java
@@ -0,0 +1,25 @@
+package org.kwakmunsu.haruhana.domain.member.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.kwakmunsu.haruhana.domain.member.repository.MemberDeviceJpaRepository;
+import org.kwakmunsu.haruhana.global.support.error.ErrorType;
+import org.kwakmunsu.haruhana.global.support.error.HaruHanaException;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class MemberDeviceValidator {
+
+    private final MemberDeviceJpaRepository memberDeviceJpaRepository;
+
+    public void validateDeleteDeviceToken(String deviceToken, Long memberId) {
+        if (memberDeviceJpaRepository.existsByMemberIdAndDeviceToken(memberId, deviceToken)) {
+            return;
+        }
+        log.warn("[MemberDeviceValidator] 회원의 디바이스 토큰이 존재하지 않아 삭제 불가 - memberId: {}, deviceToken: {}", memberId, deviceToken);
+        throw new HaruHanaException(ErrorType.NOT_FOUND_FCM_TOKEN);
+    }
+
+}
