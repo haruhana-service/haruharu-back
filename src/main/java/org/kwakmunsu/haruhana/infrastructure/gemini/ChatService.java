0ba73b55d291b3523f62642b86a1d3b8d3bcf818
kwakseobang <iii6602@naver.com>
2026-02-21 15:41:13 +0900

    Refactor[#104]: ChatService에서 RestClientException에 대한 재시도 로직 개선

--- src/main/java/org/kwakmunsu/haruhana/infrastructure/gemini/ChatService.java
+++ src/main/java/org/kwakmunsu/haruhana/infrastructure/gemini/ChatService.java
@@ -6,6 +6,7 @@ import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +15,7 @@ public class ChatService {
     private final ChatClient chatClient;  // Spring AI 에서 제공하는 ChatClient 빈 주입
 
     @Retryable(
-            retryFor = {Exception.class},
+            retryFor = {RestClientException.class},
             maxAttempts = 2,
             backoff = @Backoff(delay = 1000)
     )
