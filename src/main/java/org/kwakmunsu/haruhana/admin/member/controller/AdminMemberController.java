9842c92112842fbde33c4bf18fcbe7e8e98b9638
곽문수 <iii6602@naver.com>
2026-02-26 12:07:12 +0900

    Feat:  회원 역할 업데이트 API 구현 (#123)
    * Refactor[#118]: ROLE_GUEST 권한 삭제 및 관련 파일 정리
    
    * Feat[#118]: 회원 역할 업데이트 API 구현

--- src/main/java/org/kwakmunsu/haruhana/admin/member/controller/AdminMemberController.java
+++ src/main/java/org/kwakmunsu/haruhana/admin/member/controller/AdminMemberController.java
@@ -1,6 +1,8 @@
 package org.kwakmunsu.haruhana.admin.member.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.kwakmunsu.haruhana.admin.member.controller.dto.MemberUpdateRoleRequest;
 import org.kwakmunsu.haruhana.admin.member.enums.SortBy;
 import org.kwakmunsu.haruhana.admin.member.service.AdminMemberService;
 import org.kwakmunsu.haruhana.admin.member.service.dto.AdminMemberPreferenceResponse;
@@ -8,9 +10,12 @@ import org.kwakmunsu.haruhana.admin.member.service.dto.AdminMemberPreviewRespons
 import org.kwakmunsu.haruhana.global.support.OffsetLimit;
 import org.kwakmunsu.haruhana.global.support.response.ApiResponse;
 import org.kwakmunsu.haruhana.global.support.response.PageResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,6 +48,17 @@ public class AdminMemberController extends AdminMemberDocsController {
         AdminMemberPreferenceResponse response = adminMemberService.findMemberPreference(memberId);
 
         return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
+    @Override
+    @PatchMapping("/v1/admin/members/{memberId}/roles")
+    public ResponseEntity<ApiResponse<?>> updateMemberRole(
+            @PathVariable Long memberId,
+            @Valid @RequestBody MemberUpdateRoleRequest request
+    ) {
+        adminMemberService.updateRole(memberId,request.role());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ApiResponse.success());
     }
 
 }
